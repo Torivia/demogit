2d6dc02444a183a063702f8590c584c85940fe9f
Torivia <tlvargasrubio@gmail.com>
2024-09-25 13:59:55 -0700

    Changed stuffs

--- gitDemo/src/Main.java
+++ gitDemo/src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("Hello world!");
         //create a variable that describes what you think of coding so far
         String iThinkCodingIs = "super duper kewl";
+        boolean doYouGuysLikeItSoFar = true;
+
     }
 }
