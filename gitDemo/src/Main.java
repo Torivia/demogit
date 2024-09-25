6e99368e1613a2bfa0824583662eb86c061be254
Torivia <tlvargasrubio@gmail.com>
2024-09-25 14:13:47 -0700

    changgeee

--- gitDemo/src/Main.java
+++ gitDemo/src/Main.java
@@ -4,6 +4,6 @@ public class Main {
         //create a variable that describes what you think of coding so far
         String iThinkCodingIs = "super dUper kewl";
         boolean doYouGuysLikeItSoFar = true;
-        //new staging commit and ready to push GTITBHUB online
+        //newstaging commit and ready to push GTITBHUB online
     }
 }
